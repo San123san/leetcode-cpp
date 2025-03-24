ba4704e7941a311fdfdc4cf8fe372fdf61bac3c4
San123san <147592258+San123san@users.noreply.github.com>
2025-03-24 22:31:07 +0530

    Hard(array)

--- 42. Trapping Rain Water.cpp
+++ 42. Trapping Rain Water.cpp
@@ -31,3 +31,31 @@ public:
         return res;
     }
 };
+
+// time complexity - O(n) 
+//space complexity - O(1)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0;
+        int right = n-1;
+        int left_max = 0;
+        int right_max = 0;
+        int sum = 0;
+
+        while(left < right){
+            left_max = max(left_max, height[left]);
+            right_max = max(right_max, height[right]);
+
+            if(left_max < right_max){
+                sum += left_max - height[left];
+                left++;
+            }else{
+                sum += right_max - height[right];
+                right--;
+            }
+        }
+        return sum;
+    }
+};
