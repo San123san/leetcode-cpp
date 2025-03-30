cd1a880a3e673e34dd055c028e3047519a98bb63
San123san <147592258+San123san@users.noreply.github.com>
2025-03-30 06:59:30 +0530

    Easy(array)

---
+++ 125. Valid Palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        s.erase(remove_if(s.begin(), s.end(), [](char c) {
+            return !isalnum(c);  
+        }), s.end());
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+        int right = s.length() - 1;
+        int left = 0;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return 0;
+            }
+            left++;
+            right--;
+        }
+        return 1;
+    }
+};
