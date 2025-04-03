e997086313f6aac6f9da3d88698235b9d4f4e8d5
San123san <147592258+San123san@users.noreply.github.com>
2025-04-03 06:30:09 +0530

    String(Medium)

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -27,3 +27,44 @@ public:
         return findPalindrome(s, 0, n - 1, longestPalindrome);
     }
 };
+
+
+//
+class Solution {
+public:
+    int t[1001][1001];
+
+    bool solve(string& s, int i, int j) {
+        if (i >= j) {
+            return true;
+        }
+
+        if (t[i][j] != -1) {
+            return t[i][j];
+        }
+
+        if (s[i] == s[j]) {
+            return t[i][j] = solve(s, i + 1, j - 1);
+        }
+
+        return t[i][j] = 0;
+    }
+    string longestPalindrome(string s) {
+        int n = s.length();
+        memset(t, -1, sizeof(t));
+        int maxLen = INT_MIN;
+        int sp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(s, i, j) == true) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        sp = i;
+                    }
+                }
+            }
+        }
+        return s.substr(sp, maxLen);
+    }
+};
