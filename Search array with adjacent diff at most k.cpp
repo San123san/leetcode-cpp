5f3c646d459e6557e925ddd3b05ceedd1c1b86fe
San123san <147592258+San123san@users.noreply.github.com>
2025-07-07 10:13:40 +0530

    search and sort (Easy)

---
+++ Search array with adjacent diff at most k.cpp
@@ -0,0 +1,19 @@
+// x is the element to be searched in arr[0..n-1]
+// such that all elements differ by at-most k.
+
+class Solution {
+  public:
+    int findStepKeyIndex(vector<int>& arr, int k, int x) {
+        // code here
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] == x) {
+                return i;
+            }
+            
+            if(i + 1 < arr.size() && abs(arr[i+1] - arr[i] > k)) {
+                break;
+            } 
+        } 
+        return -1;
+    }
+};
