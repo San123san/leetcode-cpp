3a0c60afbc0edabf0408f35441e11b7902aeb2ba
San123san <147592258+San123san@users.noreply.github.com>
2025-07-08 09:17:57 +0530

    search and sort (Medium)

---
+++ Minimum Swaps to Sort.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    // Function to find the minimum number of swaps required to sort the array.
+    int minSwaps(vector<int>& arr) {
+        // Code here
+        vector<int> temp(arr.begin(), arr.end());
+        sort(temp.begin(), temp.end());
+        
+        unordered_map<int, int> pos;
+        for(int i = 0; i < arr.size(); i++) {
+            pos[arr[i]] = i;
+        }
+        
+        int swaps = 0;
+        for(int i = 0; i < arr.size(); i++){
+            if (temp[i] != arr[i]){
+                int ind = pos[temp[i]];
+                swap(arr[i], arr[ind]);
+                
+                pos[arr[i]] = i;
+                pos[arr[ind]] = ind;
+                swaps++;
+            }
+        }
+        return swaps;
+    }
+};
