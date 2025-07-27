82b248fa32cca62d70dca452e099290bfa209fcd
San123san <147592258+San123san@users.noreply.github.com>
2025-07-27 09:33:20 +0530

    Easy(BinaryTree)

---
+++ 144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+       stack<TreeNode*> stack;
+        vector<int> res;
+
+        if (root == NULL) return {};
+        TreeNode* curr = root;
+        stack.push(root);
+
+        while (!stack.empty()) {
+            curr = stack.top();
+            stack.pop();
+            res.push_back(curr->val);
+
+            if (curr->right) {
+                stack.push(curr->right);
+            }
+            if (curr->left) {
+                stack.push(curr->left);
+            }
+        }
+        return res; 
+    }
+};
