b314c9fd24bb4a6685e2b219fcf81dac597f020f
San123san <147592258+San123san@users.noreply.github.com>
2025-10-22 11:35:39 +0530

    Medium(Stack and Queues) GFG

--- The Celebrity Problem.cpp
+++ The Celebrity Problem.cpp
@@ -2,19 +2,26 @@ class Solution {
   public:
     int celebrity(vector<vector<int>>& mat) {
         // code here
-        int n = mat.size();
-        int candidate = 0;
-        for (int i = 1; i < n; i++) {
-            if (mat[candidate][i] == 1) {
-                candidate = i;
-            }
-        }
+         int n = mat.size();
+        stack<int> st;
         for (int i = 0; i < n; i++) {
-            if (i == candidate) continue;
-            if (mat[candidate][i] == 1 || mat[i][candidate] == 0) {
-                return -1;
+            st.push(i);
+        }
+        while (st.size() > 1) {
+            int a = st.top(); st.pop();
+            int b = st.top(); st.pop();
+            if (mat[a][b] == 1) {
+                st.push(b);
+            } else {
+                st.push(a);
             }
         }
-        return candidate;
+        int c = st.top();
+        for (int i = 0; i < n; i++) {
+            if (i == c) continue;
+            if (mat[c][i] == 1 || mat[i][c] == 0)
+                return -1;
+        }
+        return c;
     }
 };
